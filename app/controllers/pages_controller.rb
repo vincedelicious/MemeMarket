e036f057a6d357b0ccb9d32de874aa3be3036d7d
Rylan McNeil <rylanmcneil@hotmail.com>
2022-09-08 12:39:49 +1000

    basic backend functionality for dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,17 @@ class PagesController < ApplicationController
   def dashboard
     @memes = Meme.where { |meme| meme.user == current_user }
   end
+
+  def my_memes
+    @memes = Meme.where { |meme| meme.user == current_user }
+  end
+
+  def active_requests
+
+  end
+
+  def pending_requests
+    @bookings = Booking.where(status: 0)
+    @memes = Meme.all
+  end
 end
