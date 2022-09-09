347e4f061026e89db14a074e2591f7b5bff2fa7a
Taneshwar Sodhi <taneshwar12@gmail.com>
2022-09-09 15:57:58 +1000

    Stupid Fix

--- db/schema.rb
+++ db/schema.rb
@@ -65,23 +65,3 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_235723) do
     t.index ["user_id"], name: "index_memes_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.boolean "is_owner", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "bookings", "memes"
-  add_foreign_key "bookings", "users"
-  add_foreign_key "memes", "users"
-end
